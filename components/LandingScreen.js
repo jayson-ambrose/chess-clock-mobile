eba4dca9b00ac00d32d29924af3aad6aebb35a37
jayson <jayson.ambrose.87@gmail.com>
2023-09-06 12:53:38 -0600

    test

--- components/LandingScreen.js
+++ components/LandingScreen.js
@@ -1,38 +1,12 @@
-import React, {useEffect, useState} from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
-import { useTimer, useDelay } from 'react-use-precision-timer';
 
 export default function LandingScreen({ navigation }) {
-
-    const [clock, setClock] = useState("stopped")
-
-    const callback = () => {
-        console.log('holy cow')        
-    }
-
-    const timer = useTimer({ delay: 10000, runOnce: true, startImmediately: false }, callback);
-
     
-    useEffect(() => {
-        const interval = setInterval(() => {
-            //do the thing to check the timer -- update timer state
-            
-            setClock(timer.getRemainingTime)
-            console.log(timer.getRemainingTime())
 
-        }, 100)
-
-        return ()=> clearInterval(interval)
-
-    }, [])
-    
     return (
         <View>
-            <Text>{(clock / 1000).toFixed(2)}</Text>
-            <Button title='Start' onPress={() => timer.start()}/>
-            <Button title='Pause' onPress={() => timer.pause()}/>
-            <Button title='Resume' onPress={() => timer.resume()}/>
-            <Button title='Stop' onPress={() => timer.stop()}/>
+            <Text>Hello World</Text>
         </View>
     )
 }
